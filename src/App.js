521caee1ed605e05fb5956063612aac260d623eb
hapx2yuki <hapx2yuki@gmail.com>
2018-10-02 15:47:52 +0900

    ndjsof

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 }
+    { name: "Hanako", age: 5 },
+    { name: "NoName", age: 3 }
   ]
   return (
     <div>
@@ -18,6 +20,11 @@ const App = () => {
 
 const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
